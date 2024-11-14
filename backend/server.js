d537fdef4d7ac4071941a0f4448a300f75ee338d
Anupavithrapb <anupavithrapb@gmail.com>
2024-11-14 23:17:57 +0530

    changes

--- backend/server.js
+++ backend/server.js
@@ -3,27 +3,27 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const menuRoutes = require('./Routes/menu');
 
+// Load environment variables from .env file
+require('dotenv').config();
+
 const app = express();
 app.use(cors()); // Enable CORS for all routes
-
-
 const corsOptions = {
   origin: process.env.FRONTEND_URL, 
-  methods: ['GET', 'POST','PATCH','PUT','DELETE'],
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
-            
 };
 
 app.use(cors(corsOptions));
 
-// Connection string with database name specified and without special characters in password
-mongoose.connect('mongodb+srv://anupavithrapb:Anu%402000@cluster0.cauyb.mongodb.net/deepnet', {
+// MongoDB connection using the URI from the .env file
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch((error) => console.error('Error connecting to MongoDB:', error));
-
+app.get('/',(req,res) =>{res.send("data")})
 app.use('/api', menuRoutes);
 
 const PORT = process.env.PORT || 5000;
